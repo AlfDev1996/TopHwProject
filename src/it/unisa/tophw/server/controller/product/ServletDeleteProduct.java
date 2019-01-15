ffa8555c1f71310198dc64853d51338e63a1ef6a
RaffaeleDragone <raffidragone@gmail.com>
2019-01-15 11:18:50 +0100

    Commit Servlet Product

--- src/it/unisa/tophw/server/controller/product/ServletDeleteProduct.java
+++ src/it/unisa/tophw/server/controller/product/ServletDeleteProduct.java
@@ -1,5 +1,9 @@
 package it.unisa.tophw.server.controller.product;
 
+import it.unisa.tophw.server.model.beans.ProductBean;
+import it.unisa.tophw.server.model.management.ProductDAO;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +14,34 @@ import java.io.IOException;
 @WebServlet(name = "ServletDeleteProduct")
 public class ServletDeleteProduct extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        String nome = request.getParameter("Nome_Prodotto").trim();
+        int idProdotto = Integer.parseInt( !request.getParameter("id_prodotto").isEmpty() ? request.getParameter("id_prodotto") : "0");
+
+        ProductBean prodotto=new ProductBean();
+        ProductDAO prodottoDAO=new ProductDAO();
+        if(idProdotto>0){
+            prodotto= prodottoDAO.doRetriveById(idProdotto);
+        }else{
+            prodotto = prodottoDAO.doRetriveByNome(nome);
+        }
+
+        boolean res = false;
+        if(prodotto!=null && prodotto.getId_prodotto()>0){
+            res = prodottoDAO.doDelete(prodotto.getId_prodotto());
+        }
+
+        String msgOutput="";
+        if(res){
+            msgOutput="ok";
+        }else{
+            msgOutput="Errore durante l'eliminazione del prodotto";
+        }
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/ModificaArticolo.jsp?msgOutput="+msgOutput+"");
+        dispatcher.forward(request, response);
+
 
     }
 
--- src/it/unisa/tophw/server/controller/product/ServletUpdateProduct.java
+++ src/it/unisa/tophw/server/controller/product/ServletUpdateProduct.java
@@ -1,5 +1,9 @@
 package it.unisa.tophw.server.controller.product;
 
+import it.unisa.tophw.server.model.beans.ProductBean;
+import it.unisa.tophw.server.model.management.ProductDAO;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +14,42 @@ import java.io.IOException;
 @WebServlet(name = "ServletUpdateProduct")
 public class ServletUpdateProduct extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nome = request.getParameter("Nome_Prodotto").trim();
+        String descrizione_b = request.getParameter("Descrizione_Breve").trim();
+        String descrizione = request.getParameter("Descrizione").trim();
+        Double prezzo = Double.parseDouble(request.getParameter("Prezzo"));
+        String marca = request.getParameter("marca");
+        int quantita=Integer.parseInt(request.getParameter("quantita"));
+        int idProdotto = Integer.parseInt( !request.getParameter("id_prodotto").isEmpty() ? request.getParameter("id_prodotto") : "0");
 
+        ProductBean prodotto=new ProductBean();
+        ProductDAO prodottoDAO=new ProductDAO();
+        if(idProdotto>0){
+            prodotto= prodottoDAO.doRetriveById(idProdotto);
+        }else{
+            prodotto = prodottoDAO.doRetriveByNome(nome);
+        }
+
+        prodotto.setNome(nome);
+        prodotto.setDescrizione_breve(descrizione_b);
+        prodotto.setDescrizione_estesa(descrizione);
+        prodotto.setPrezzo(prezzo);
+        prodotto.setQuantita(quantita);
+
+        boolean res = false;
+        if(prodotto!=null && prodotto.getId_prodotto()>0){
+           res = prodottoDAO.doUpdate(prodotto);
+        }
+
+        String msgOutput="";
+        if(res){
+            msgOutput="ok";
+        }else{
+            msgOutput="Errore durante l'update";
+        }
+
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/ModificaArticolo.jsp?msgOutput="+msgOutput+"");
+        dispatcher.forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
