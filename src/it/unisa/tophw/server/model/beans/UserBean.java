57164480fd64bd02d558f8cd5792709284a3a445
RaffaeleDragone <raffidragone@gmail.com>
2019-01-21 16:49:18 +0100

    UpdateUser

--- src/it/unisa/tophw/server/model/beans/UserBean.java
+++ src/it/unisa/tophw/server/model/beans/UserBean.java
@@ -4,12 +4,12 @@ public class UserBean {
 
     private int id_utente;
     private String nome,cognome,email,password,ruolo,nazione;
-    private AddressBean indirizzo;
+    //private AddressBean indirizzo;
 
     public UserBean() {
         id_utente=0;
         nome=cognome=email=password=ruolo=nazione="";
-        indirizzo=new AddressBean();
+        //indirizzo=new AddressBean();
     }
 
     public UserBean(int id_utente, String nome, String cognome, String email, String password, String ruolo, String nazione, AddressBean indirizzo) {
@@ -20,16 +20,20 @@ public class UserBean {
         this.password = password;
         this.ruolo = ruolo;
         this.nazione = nazione;
-        this.indirizzo=indirizzo;
+        //this.indirizzo=indirizzo;
     }
 
+  /*
     public AddressBean getIndirizzo() {
+
         return indirizzo;
     }
-
+    */
+/*
     public void setIndirizzo(AddressBean indirizzo) {
         this.indirizzo = indirizzo;
     }
+    */
 
     public int getId_utente() {
         return id_utente;
