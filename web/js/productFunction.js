955bc5dbd61538a70ffd843ce851621e8f0ec594
AlfDev1996 <33827710+AlfDev1996@users.noreply.github.com>
2019-01-23 13:59:24 +0100

    Pannello Amministratore

--- web/js/productFunction.js
+++ web/js/productFunction.js
@@ -61,8 +61,10 @@ function loadCatalog(){
             var response=xh.responseText;
 
             var catalogJson= JSON.parse(response);
-
+            console.log(catalogJson);
             var select = document.getElementsByName("selectCatalog");
+
+
 
             for(var j=0; j<select.length;++j) {
 
@@ -76,7 +78,7 @@ function loadCatalog(){
 
                     option.value = catalogJson[i].id_catalogo;
 
-                    console.log(catalogJson);
+
                     option.setAttribute("title", catalogJson[i].sconto );
                     select[j].add(option);
 
@@ -96,9 +98,61 @@ function loadCatalog(){
 
 
 function updateField(object){
-    var currentCatalog= object.options[object.selectedIndex].text;
+    var idCatalogo= object.options[object.selectedIndex].id;
     var n_catalog= document.getElementById("mod_namecatalog");
-    n_catalog.setAttribute("value",""+currentCatalog);
-    console.log(object.options[object.selectedIndex].title);
+    var desc_catalogo = document.getElementById("mod_descrizionecatalogo");
+    var sconto_catalogo= document.getElementById("modscontocatalogo");
+    var nomeCatalogo ="";
+    var descCatalog="";
+    var sconto="";
+
+    var xh= new XMLHttpRequest;
+
+    xh.onreadystatechange=function(){
+
+
+
+        if(xh.readyState==4 && xh.status==200){
+
+            var response=xh.responseText;
+
+            var catalogJson= JSON.parse(response);
+
+
+
+
+
+
+                for (var i = 0; i < catalogJson.length; ++i) {
+
+                    if(idCatalogo== catalogJson[i].id_catalogo){
+                            if(catalogJson[i].nomeCatalogo != null && catalogJson[i].nomeCatalogo != undefined )
+                            nomeCatalogo=catalogJson[i].nomeCatalogo;
+                            console.log( catalogJson[i].descrizioneCatalogo);
+
+
+                        if(catalogJson[i].descrizioneCatalogo != null && catalogJson[i].descrizioneCatalogo != undefined )
+                            descCatalog= catalogJson[i].descrizioneCatalogo;
+                        if(catalogJson[i].sconto != null && catalogJson[i].sconto != undefined )
+                            sconto = catalogJson[i].sconto;
+                            n_catalog.setAttribute("value",""+nomeCatalogo);
+                            desc_catalogo.value= descCatalog;
+                            sconto_catalogo.setAttribute("value", sconto);
+                    }
+
+
+
+
+                }
+
+        }
+
+    }
+
+    xh.open("GET","ServletCatalogFindAll",true);
+
+    xh.send();
+
+
 
 }
